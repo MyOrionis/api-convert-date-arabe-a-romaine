87433029602c854106d75bc8e8a75fb66d52812c
MyOrionis <c.kleiber@cs2i-bourgogne.com>
2020-09-07 11:40:42 +0200

    test unitaire réaliser pour la convertion

--- src/convert/convert.controller.spec.ts
+++ src/convert/convert.controller.spec.ts
@@ -2,11 +2,10 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ConvertController } from './convert.controller';
 import {ConvertService} from "./convert.service";
 import * as request from 'supertest';
-import { INestApplication, HttpService,HttpModule } from '@nestjs/common';
-
-
-
-
+import {INestApplication, HttpService, HttpModule} from '@nestjs/common';
+import { AxiosResponse } from 'axios';
+import { of } from 'rxjs';
+import {AppModule} from "../app.module";
 
 
 describe('ConvertController', () => {
@@ -16,28 +15,47 @@ describe('ConvertController', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [AppModule, HttpModule],
       controllers: [ConvertController],
       providers: [ConvertService],
     }).compile();
 
+    app = module.createNestApplication();
     controller = module.get<ConvertController>(ConvertController);
-
+    httpService = module.get<HttpService>(HttpService);
+    await app.init();
   });
-  function convert(param) {
-    const options = {
-      'method': 'GET',
-      'url': 'http://localhost:3000/convert/' + param,
+
+  it('test de date incorrect', async () => {
+    const result: AxiosResponse = {
+      data: {},
+      status: 404,
+      statusText: 'Paramètre incorrecte',
+      headers: {},
+      config: {},
     };
 
+    jest.spyOn(httpService, 'get').mockImplementationOnce(() => of(result));
 
-  }
-
-  test('Paramètre incorrecte', async () => {
-    expect(await convert('non valide')).toBe(422);
+    return request(app.getHttpServer())
+      .get('/convert/23//2020')
+      .expect(404);
   });
 
-  test('Convertion correcte', async () => {
-    expect(await convert(1133)).toBe('MCXXXIII');
+  it('test de date correct', async () => {
+    const result: AxiosResponse = {
+      data: {
+        dateAR :'23/03/2020',
+        dateRo: 'XXIII/III/MMXX'},
+      status: 200,
+      statusText: 'Convertion correcte',
+      headers: {},
+      config: {},
+    };
+    jest.spyOn(httpService, 'get').mockImplementationOnce(() => of(result));
+    const response = await request(app.getHttpServer())
+      .get('/convert/23/03/2020')
+      .expect(200);
+    expect(response.text).toBe('XXIII/III/MMXX');
   });
-
 });
